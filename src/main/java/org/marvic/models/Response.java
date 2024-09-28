50b155b746ae5d329b9ef6bcc566279d64f59413
Nehemias Muñoz <nehemiasmunozm@gmail.com>
2024-09-28 16:08:29 -0300

    Add: Response model

--- src/main/java/org/marvic/models/Response.java
+++ src/main/java/org/marvic/models/Response.java
@@ -1,4 +1,24 @@
 package org.marvic.models;
 
-public class Response {
+public class Response{
+    String result;
+    String base_code;
+    String target_code;
+    double conversion_rate;
+    double conversion_result;
+
+
+    public String getBase_code() {
+        return base_code;
+    }
+
+    public String getTarget_code() {
+        return target_code;
+    }
+
+    public double getConversion_result() {
+        return conversion_result;
+    }
 }
+
+
